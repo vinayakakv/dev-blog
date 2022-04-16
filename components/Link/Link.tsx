4af37f0e49b2220bf7f80a4658e78f62ec8e41e5
Vinayaka K V <me.vinayakakv@gmail.com>
2022-04-16 11:33:02 +0530

    Implement Link

---
+++ components/Link/Link.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NextLink from 'next/link'
+
+type Props = React.HTMLProps<HTMLAnchorElement> & { external?: boolean }
+
+export function Link({ href, external = false, ...rest }: Props) {
+  const className = 'hover:text-green-400 hover:underline'
+  return external ? (
+    <a href={href} className={className} {...rest} />
+  ) : (
+    <NextLink href={href!} passHref>
+      <a className={className} {...rest} />
+    </NextLink>
+  )
+}

f102844d2a6a074e4a8f4c7547c3f4e341c8b542
Vinayaka K V <me.vinayakakv@gmail.com>
2022-04-16 11:31:14 +0530

    Update global app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Navbar } from '../components/Navbar'
+import { Footer } from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>dev.vinayakakv</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <main className="overflow-y-auto">
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+    </>
+  )
 }
 
 export default MyApp

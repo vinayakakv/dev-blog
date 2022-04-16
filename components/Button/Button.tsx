51c459a2a1cdf468415725a7488cfafbe356e28b
Vinayaka K V <me.vinayakakv@gmail.com>
2022-04-16 11:31:48 +0530

    Implement Button

---
+++ components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = React.HTMLProps<HTMLButtonElement> & {
+  icon?: React.ReactNode
+}
+
+export function Button({ children, icon, className, type, ...rest }: Props) {
+  return (
+    <button
+      type="button"
+      {...rest}
+      className={`${className} flex flex-row items-center justify-center gap-4 border-2 border-white px-4 py-2 hover:border-green-400 hover:text-green-400`}
+    >
+      {!!icon && icon}
+      {children}
+    </button>
+  )
+}

477cb7e1da37608a6de077b723a779daaceb4458
Vinayaka K V <me.vinayakakv@gmail.com>
2022-04-16 12:43:10 +0530

    Implement External link opening in new tab

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -6,7 +6,7 @@ type Props = React.HTMLProps<HTMLAnchorElement> & { external?: boolean }
 export function Link({ href, external = false, ...rest }: Props) {
   const className = 'hover:text-green-400 hover:underline'
   return external ? (
-    <a href={href} className={className} {...rest} />
+    <a href={href} className={className} target="_blank" {...rest} />
   ) : (
     <NextLink href={href!} passHref>
       <a className={className} {...rest} />

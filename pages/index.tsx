3100989e02f286f35a8825cef26bd09e4e0474f9
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-05 13:55:15 +0530

    Implement blog content from GraphCMS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { gql } from '@apollo/client'
 import type { InferGetStaticPropsType, NextPage } from 'next'
 import { BlogCard } from '../components/BlogCard'
 import { Hero } from '../components/Hero'
 import { Link } from '../components/Link'
 import { Section } from '../components/Section'
+import client from '../helpers/graphql'
+import { Post } from '../schema/post'
 
 const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   posts,
@@ -17,14 +20,14 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
         <div className="grid grid-flow-row grid-cols-1 gap-2 md:grid-cols-2">
           {posts.map((post) => (
             <BlogCard
-              title={post.name}
+              title={post.title}
               description={post.description}
               date={post.date}
               slug={post.slug}
               key={post.slug}
-              tags={post.tags.map((tag) => ({
-                name: tag,
-                slug: tag.toLocaleLowerCase(),
+              tags={post.categories.map((category) => ({
+                name: category.title,
+                slug: category.slug.toLocaleLowerCase(),
               }))}
             />
           ))}
@@ -38,38 +41,23 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
 export default Home
 
 export const getStaticProps = async () => {
-  const posts = [
-    {
-      name: 'From No-Code to Code',
-      description:
-        'Experience of migrating a E-Commerce website from No-Code SaaS to NextJS',
-      slug: 'no-code',
-      date: '2022-Dec-20',
-      tags: ['NextJS', 'React', 'TypeScript'],
-    },
-    {
-      name: 'Pulumifying Mainteny',
-      description: 'What to took to bring entire infra of Mainteny to IaC',
-      slug: 'pulumify',
-      date: '2022-Dec-20',
-      tags: ['Pulumi', 'IaC', 'TypeScript'],
-    },
-    {
-      name: 'State-based Programming',
-      description: 'Wild ideas on how State-based programming can be used',
-      slug: 'state-programming',
-      date: '2022-Dec-20',
-      tags: ['Pulumi', 'Programming'],
-    },
-    {
-      name: 'I built my website',
-      description: 'How I built my website using Gatsby',
-      slug: 'i-built-website',
-      date: '2022-Dec-20',
-      tags: ['Gatsby', 'React'],
-    },
-  ]
+  const { data } = await client.query<{ posts: Post[] }>({
+    query: gql`
+      query {
+        posts {
+          slug
+          title
+          description
+          date
+          categories {
+            slug
+            title
+          }
+        }
+      }
+    `,
+  })
   return {
-    props: { posts },
+    props: { posts: data.posts },
   }
 }

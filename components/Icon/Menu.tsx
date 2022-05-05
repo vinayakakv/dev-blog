a3855df10f6a0e2dbd81fce12585cda4e80c09ef
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-05 12:21:46 +0530

    Fix attribute name

--- components/Icon/Menu.tsx
+++ components/Icon/Menu.tsx
@@ -5,7 +5,7 @@ export function Menu() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
+      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     >

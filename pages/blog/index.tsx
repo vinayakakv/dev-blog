5c516c908dcbb0a72f46e21cdffc3a3f1fb25854
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-05 16:34:53 +0530

    Implement Blog list page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,66 @@
+import { gql } from '@apollo/client'
+import type { InferGetStaticPropsType, NextPage } from 'next'
+import { BlogCard } from '@components/BlogCard'
+import { Section } from '@components/Section'
+import client from '@helpers/graphql'
+import { Post } from '@schema/post'
+import { Placeholder } from '@components/Placeholder'
+
+const Blog: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  posts,
+}) => {
+  return (
+    <>
+      <Section
+        big
+        name="Blog"
+        description="I write about the projects I've worked on, and my experience and learnings along the way"
+        className="py-10"
+      >
+        <div className="grid grid-flow-row grid-cols-1 gap-2 md:grid-cols-2">
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <BlogCard
+                title={post.title}
+                description={post.description}
+                date={post.date}
+                slug={`/blog/${post.slug}`}
+                key={post.slug}
+                tags={post.categories.map((category) => ({
+                  name: category.title,
+                  slug: category.slug.toLocaleLowerCase(),
+                }))}
+              />
+            ))
+          ) : (
+            <Placeholder>Coming Soon!</Placeholder>
+          )}
+        </div>
+      </Section>
+    </>
+  )
+}
+
+export default Blog
+
+export const getStaticProps = async () => {
+  const { data } = await client.query<{ posts: Post[] }>({
+    query: gql`
+      query {
+        posts(orderBy: date_DESC) {
+          slug
+          title
+          description
+          date
+          categories {
+            slug
+            title
+          }
+        }
+      }
+    `,
+  })
+  return {
+    props: { posts: data.posts },
+  }
+}

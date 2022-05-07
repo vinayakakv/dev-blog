0270480ac509cdb437dd04e6e7f129aa07c9207c
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-07 10:48:37 +0530

    Implement active route highlighting

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { Button } from '../Button'
 import { Icon } from '../Icon'
@@ -16,10 +17,12 @@ const menuLinks = [
 
 export function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false)
+  const { pathname } = useRouter()
+  const activeRoute = pathname.split('/').slice(0, 2).join('/')
   const menuItems = (
     <menu className="flex flex-col items-center gap-6 sm:flex-row">
       {menuLinks.map(({ name, link }) => (
-        <li key={link}>
+        <li key={link} className={link === activeRoute ? 'font-semibold' : ''}>
           <Link href={link}>{name}</Link>
         </li>
       ))}

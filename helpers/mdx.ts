5a4ccc008bf8d8ce8ba2b3f5e784ba371b100155
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-08 17:00:35 +0530

    Add emoji support in MDX

--- helpers/mdx.ts
+++ helpers/mdx.ts
@@ -1,3 +1,9 @@
 import { serialize as mdxSeralize } from 'next-mdx-remote/serialize'
+import emoji from 'remark-emoji'
 
-export const serialize = async (mdx: string) => mdxSeralize(mdx)
+export const serialize = async (mdx: string) =>
+  mdxSeralize(mdx, {
+    mdxOptions: {
+      remarkPlugins: [emoji],
+    },
+  })

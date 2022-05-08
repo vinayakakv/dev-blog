a7fc62a0b4401f25af8907b941edd3fa58cfdf36
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-08 20:07:21 +0530

    Add smartypants remark plugin

--- helpers/mdx.ts
+++ helpers/mdx.ts
@@ -1,9 +1,10 @@
 import { serialize as mdxSeralize } from 'next-mdx-remote/serialize'
 import emoji from 'remark-emoji'
+import smartypants from 'remark-smartypants'
 
 export const serialize = async (mdx: string) =>
   mdxSeralize(mdx, {
     mdxOptions: {
-      remarkPlugins: [emoji],
+      remarkPlugins: [emoji, smartypants],
     },
   })

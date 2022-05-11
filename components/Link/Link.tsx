400c07fea67e5310adc5ff9121e3a3511b710834
Vinayaka K V <me.vinayakakv@gmail.com>
2022-05-11 13:25:47 +0530

    Fix empty className in Link

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -3,7 +3,12 @@ import NextLink from 'next/link'
 
 type Props = React.HTMLProps<HTMLAnchorElement> & { external?: boolean }
 
-export function Link({ href, external = false, className, ...rest }: Props) {
+export function Link({
+  href,
+  external = false,
+  className = '',
+  ...rest
+}: Props) {
   const finalClassName = `text-green-400 hover:underline ${className}`
   return external ? (
     <a href={href} className={finalClassName} target="_blank" {...rest} />
